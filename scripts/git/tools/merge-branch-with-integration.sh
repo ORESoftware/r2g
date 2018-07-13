91662967175057b7456b803a32e0b53786493700
Olegzandr Denman <alex@oresoftware.com>
2018-07-13 15:45:27 -0700

    pdev:'set'

--- scripts/git/tools/merge-branch-with-integration.sh
+++ scripts/git/tools/merge-branch-with-integration.sh
@@ -17,6 +17,9 @@ git add -A
 git commit --allow-empty -am "merge_at:${time_seconds}"
 
 
-git merge -Xignore-space-change --no-ff "origin/dev" # use --no-ff to force a new commit
+git merge -Xignore-space-change  "origin/dev" # use --no-ff to force a new commit
 git push origin HEAD
 
+
+git checkout dev; # so we don't keep working on the old feature branch
+git merge -Xignore-space-change origin/dev;
