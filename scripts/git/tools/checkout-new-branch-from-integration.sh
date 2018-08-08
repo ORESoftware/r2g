ac3916253316542b2caa246beda4a0a9649d0aa5
Olegzandr VD <alex@oresoftware.com>
2018-08-07 19:03:38 -0700

    pdev:'set'

--- scripts/git/tools/checkout-new-branch-from-integration.sh
+++ scripts/git/tools/checkout-new-branch-from-integration.sh
@@ -25,7 +25,6 @@ if ! contains "$branch_type" "${arr[@]}"; then
     exit 1;
 fi
 
-
 git fetch origin;
 
 time_seconds=`node -e 'console.log(String(Date.now()).slice(0,-3))'`;
@@ -35,6 +34,6 @@ new_branch="${USER}/${branch_type}/${time_seconds}"
 
 echo "New branch name: $new_branch";
 
-git branch --no-track "${new_branch}" "origin/dev"
+git branch --no-track "${new_branch}" "remotes/origin/dev"
 git checkout "${new_branch}"
 git push -u origin HEAD  # makes sure git is tracking this branch on the primary remote
